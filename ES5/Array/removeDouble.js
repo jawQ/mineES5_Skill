47030b19123a857d80e710db662f6e18b52c44af
wjq <jawq@foxmail.com>
2018-03-30 11:59:34 +0800

    更新了去重的方法

--- ES5/Array/removeDouble.js
+++ ES5/Array/removeDouble.js
@@ -1,18 +1,25 @@
-var _arr = [5, 2, 1, 2, 4, 3, 5, 6, 7, 1, 0];
+var _arr = [5, 2, 1, 2, 4, 3, 5, 6, 7, 1, 0, 6, 7, 1, 0, 5, 6, 7,1,1];
 function removeDoubel(array) {
+  console.log('这是未去重的数组')
+  console.log(array)
+  
+  
   var i,
     k,
-    len = array.length;
+    len = array.length
   for (i = 0; i < len; i++) {
     debugger
-    for (k = i+1; k < len; k++) {
-      if (array[i] == array[k]) {
+    for (k = len-1; k > i; k--) {
+      var jawq_i = array[i];
+      var jawq_k = array[k];
+      if (jawq_i == jawq_k) {
         array.splice(k,1);
         len--;
-        k--;
+        // k++;
       }
     }
   }
   return array;
 }
+console.log('这是已经去重的数组')
 console.log(removeDoubel(_arr));
